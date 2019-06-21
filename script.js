2af09c6f66e654d40b75a998e77d0512141dcfdf
arstrel <arstrel@gmail.com>
2019-06-21 14:24:35 -0400

    Refactored

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
-const elementWithTheGuy = $('#little-guy')
-//find the span with this class and append little-guy to this element
 let row = 1;
-let col = 1;
-let coordsClass = `r${row}c${col}`
+let col = 1
 document.onkeydown = function(e) {
-  if(e.key == "ArrowUp") {
-    if (row > 1) {
-      console.log(`moving the little guy up`);
+  if(e.key == "ArrowUp" && row > 1) {
       row -= 1;
       moveTheLittleGuy(row, col)
-    }
   }
-  if(e.key == "ArrowDown") {
-    if (row < 10 ) {
-      console.log(`moving the little guy down`);
+  if(e.key == "ArrowDown" && row < 10) {
       row += 1;
       moveTheLittleGuy(row, col)
-    }
   }
-  if(e.key == "ArrowLeft") {
-    if(col > 1) {
-      console.log(`moving the little guy left`);
+  if(e.key == "ArrowLeft" && col > 1) {
       col -= 1;
       moveTheLittleGuy(row, col)
-    }
   }
-  if(e.key == "ArrowRight") {
-    if(col < 10) {
-      console.log(`moving the little guy right`);
+  if(e.key == "ArrowRight" && col < 10) {
       col += 1;
       moveTheLittleGuy(row, col)
-    }
   }
 }
 function moveTheLittleGuy (rowToMoveto, colToMoveTo) {
   let coordsClass = `.r${rowToMoveto}c${colToMoveTo}`
-  console.log(coordsClass);
   $(coordsClass).append($('#little-guy'))
 }
